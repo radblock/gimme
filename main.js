77070b669d0ddd05ad15b6faed421b024a6ed6ac
Andrew Monks <a@monks.co>
2016-04-25 22:54:23 -0500

    claudia

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// gimme/main.js
-
 'use strict'
 
 var aws = require('aws-sdk')
@@ -14,36 +12,42 @@ var deps = {
 module.exports = api
 
 api.post('/sign', function (request) {
-  console.log('request body: ', request.body)
-  // {name, type}
+  return new Promise(function (resolve, reject) {
+    console.log('request body: ', request.body)
+    // {name, type}
 
-  var gen_random_string = function rrr (n) {
-    var s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    return Array.apply(null, Array(n))
-      .map(function () {
-        return s.charAt(Math.floor(Math.random() * s.length))
-      }).join('')
-  }
-
-  var upload_key = gen_random_string(4) + '-' + request.body.name
-
-  var s3 = new aws.S3()
-  var s3_params = {
-    Bucket: deps.bucket,
-    Key: upload_key,
-    Expires: 60,
-    ContentType: request.body.type,
-    ACL: 'public-read'
-  }
-  console.log(s3_params)
-  s3.getSignedUrl('putObject', s3_params, function (err, data) {
-    if (err) {
-      return err
+    if (request.body.type !== 'image/gif') {
+      reject('must be a gif')
     }
-    var return_data = {
-      signed_request: data
+
+    var gen_random_string = function rrr (n) {
+      var s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+      return Array.apply(null, Array(n))
+        .map(function () {
+          return s.charAt(Math.floor(Math.random() * s.length))
+        }).join('')
     }
-    return return_data
+
+    var upload_key = gen_random_string(4) + '-' + request.body.name
+
+    var s3 = new aws.S3()
+    var s3_params = {
+      Bucket: deps.bucket,
+      Key: upload_key,
+      Expires: 60,
+      ContentType: request.body.type,
+      ACL: 'public-read'
+    }
+    console.log(s3_params)
+    s3.getSignedUrl('putObject', s3_params, function (err, data) {
+      if (err) {
+        reject(err)
+      }
+      var return_data = {
+        signed_request: data
+      }
+      resolve(return_data)
+    })
   })
 })
 
