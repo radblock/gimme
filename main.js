4a6aaa2160064884e0b48e655d6c08b36dd07b92
Andrew Monks <a@monks.co>
2016-05-07 01:36:50 -0500

    validation

--- main.js
+++ main.js
@@ -1,90 +1,110 @@
 'use strict'
 
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * SETUP
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- */
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 var aws = require('aws-sdk')
 var s3 = new aws.S3()
+
+var js_schema = require('js-schema')
 
 var ApiBuilder = require('claudia-api-builder')
 var api = new ApiBuilder()
 module.exports = api
 
-// https://github.com/radblock/lambda-s3-authenticator
-var authenticator = require('lambda-s3-authenticator')
-
 // var deps = require('./deps.json')
 var deps = {
+  'user_bucket': 'radblock-users',
   'bucket': 'gifs.radblock.xyz',
   'pending_bucket': 'radblock-pending-gifs'
 }
 
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * API
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- */
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/*  request = {
- *    email,
- *    password,
- *    url,
- *    gif
- *  }
- */
 api.post('/submit', function (request) {
+  validate({
+    email: String,
+    password: String,
+    url: String,
+    gif: String
+  }, request)
   return new Promise(function (resolve, reject) {
-    return authenticator.go_create_or_find(request.body)
+    return go_create_or_find_user(request.body)
            .then(randomize_filename)
            .then(go_handle_upload)
+           .then(go_validate({
+             signed_request: String,
+             bucket: [deps.bucket, deps.pending_bucket],
+             key: String
+           }))
            .then(resolve)
-           .catch(reject)
+           .catch(function () {
+             reject({
+               'status': 'failure'
+             })
+           })
   })
 })
 
-/*  request = {
- *    email,
- *    code
- *  }
- */
 api.post('/verify', function (request) {
+  validate({
+    email: String,
+    code: String
+  }, request)
   return new Promise(function (resolve, reject) {
-    return authenticator.go_verify(request.body)
+    return go_verify_user(request.body)
            .then(go_unpend)
-           .then(resolve)
-           .catch(reject)
+           .then(function () {
+             resolve({
+               'status': 'success'
+             })
+           })
+           .catch(function () {
+             reject({
+               'status': 'failure'
+             })
+           })
   })
 })
 
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * FUNCTIONS
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- */
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/*  user = {
- *    gif,
- *    url,
- *    state,
- *  }
- */
 function go_handle_upload (user) {
+  validate({
+    gif: String,
+    url: String,
+    state: ['ready', 'pending', 'rate-limited', 'banned']
+  })
   return new Promise(function (resolve, reject) {
     switch (user.state) {
       case 'ready':
         go_rate_limit(user)
         go_charge_card(user)
         return go_get_signed_url_for(deps.bucket, user.gif)
+               .then(go_validate({
+                 signed_request: String,
+                 bucket: [deps.bucket, deps.pending_bucket],
+                 key: String
+               }))
                .then(resolve)
+               .catch(reject)
 
       case 'pending':
         go_rate_limit(user)
         go_charge_card(user)
         return go_get_signed_url_for(deps.pending_bucket, user.gif)
+               .then(go_validate({
+                 signed_request: String,
+                 bucket: [deps.bucket, deps.pending_bucket],
+                 key: String
+               }))
                .then(resolve)
+               .catch(reject)
 
       case 'rate-limited':
         return reject('you already uploaded a gif today.')
@@ -95,28 +115,26 @@ function go_handle_upload (user) {
   })
 }
 
-/*  user = {
- *    gif_key
- *  }
- */
 function go_unpend (user) {
+  validate({
+    gif_key: String
+  }, user)
   return new Promise(function (resolve, reject) {
-    // TODO: write this
-    // move the user's pending gif into the regular bucket and update their state accordingly
-    return resolve(user)
+    // move the user's pending gif into the regular bucket
+    s3.copyObject({
+      Bucket: deps.bucket,
+      Key: user.gif_key,
+      CopySource: `${deps.pending_bucket}/${user.gif_key}`
+    }, function (err, data) {
+      if (err) { return reject(err) }
+      return resolve(user)
+    })
   })
 }
 
-/*  user = {
- *    status
- *  }
- *  return = {
- *    status: 'rate-limited'
- *  }
- */
 function go_rate_limit (user) {
   return new Promise(function (resolve, reject) {
-    // TODO: write this
+    user.state = 'rate-limited'
     return resolve(user)
   })
 }
@@ -128,14 +146,11 @@ function go_charge_card (user) {
   })
 }
 
-/*  return = {
- *    signed_request,
- *    bucket,
- *    key
- *  }
- */
-}
 function go_get_signed_url_for (bucket, user) {
+  validate(String, bucket)
+  validate({
+    gif: String
+  }, user)
   return new Promise(function (resolve, reject) {
     s3.getSignedUrl('putObject', {
       Bucket: bucket,
@@ -153,39 +168,81 @@ function go_get_signed_url_for (bucket, user) {
   })
 }
 
-/*  request = {
- *    email,
- *    password
- *  }
- *  return = user
- }
- */
 function go_create_or_find_user (request) {
+  validate({
+    email: String,
+    password: String
+  }, request)
   return new Promise(function (resolve, reject) {
-    // try to get the user from s3
-      // if they do exist, check their password
-        // resolve if their password is right
-        // reject if their password is wrong
-      // else they don't exist, create a new user in s3 and resolve
+    go_get_user(request)
+      .catch(go_create_user)
+      .then(resolve)
+      .catch(reject)
   })
 }
 
-/*  request = {
- *    email,
- *    code
- *  }
- *  return = user
- }
- */
 function go_verify_user (request) {
+  validate({
+    email: String,
+    code: String
+  }, request)
   return new Promise(function (resolve, reject) {
-    // try to get the pending user from s3
-      // check the code if they do exist
-        // resolve if it's correct
+    go_get_user(request.email)
+      .then(function (user) {
+        if (user.code === request.code) {
+          return resolve(user)
+        }
+        return reject(user)
+      })
+      .catch(reject)
+  })
+}
+
+function go_create_user (request) {
+  validate({
+    email: String,
+    password: String
+  }, request)
+  return new Promise(function (resolve, reject) {
+    go_save_user({
+      email: request.email,
+      password: request.password
+    }).then(resolve)
+  })
+}
+
+function go_get_user (request) {
+  validate({
+    email: String
+  }, request)
+  return new Promise(function (resolve, reject) {
+    s3.getObject({
+      Bucket: deps.user_bucket,
+      Key: request.email
+    }, function (err, data) {
+      if (err) { return reject(err) }
+      return resolve(data)
+    })
+  })
+}
+
+function go_save_user (user) {
+  validate({
+    email: String
+  }, user)
+  return new Promise(function (resolve, reject) {
+    s3.putObject({
+      Bucket: deps.user_bucket,
+      Key: user.email
+    }, function (err, data) {
+      if (err) { return reject(err) }
+      return resolve(user)
+    })
   })
 }
 
 function gen_random_string (length) {
+  validate(Number, length)
   var s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
   return Array.apply(null, Array(length))
     .map(function () {
@@ -193,16 +250,29 @@ function gen_random_string (length) {
     }).join('')
 }
 
-/*  user = {
- *    filename
- *  }
- *  return = {
- *    gif_key,
- *    filename
- *  }
- */
-}
 function randomize_filename (user) {
+  validate({
+    filename: String
+  }, user)
   user.gif_key = gen_random_string(4) + '-' + gen_random_string(4) + '/' + user.filename
   return user
 }
+
+function validate (schema, input) {
+  var Schema = js_schema(schema)
+  return Schema(input)
+}
+
+function go_validate (schema) {
+  return function (input) {
+    return new Promise(function (resolve, reject) {
+      var is_validated = validate(schema, input)
+      if (is_validated) {
+        resolve(input)
+      } else {
+        reject(input)
+      }
+    })
+  }
+}
+
